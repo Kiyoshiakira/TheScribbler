368c24a47242b7aebbf880d30403b6f223ac1afe
Kiyoshi Life (Shing) <Shaunessy24@gmail.com>
2025-10-29 00:41:13 +0000

    firebaseapphosting.clients6.google.com/$rpc/google.firebase.apphosting.v

---
+++ src/app/next.config.ts
@@ -0,0 +1,65 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com;
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+      img-src 'self' blob: data: https://images.unsplash.com https://picsum.photos https://lh3.googleusercontent.com;
+      font-src 'self' https://fonts.gstatic.com;
+      connect-src 'self' https://*.google.com https://*.googleapis.com https://*.googleusercontent.com wss://*.cloudworkstations.dev;
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+    `;
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
