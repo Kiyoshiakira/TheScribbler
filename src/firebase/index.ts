55ffb1de3bb51b7cb4639fe81006ca8474aa6ed2
Kiyoshi Life (Shing) <Shaunessy24@gmail.com>
2025-11-02 19:56:44 +0000

    We should move to the next task on the editor list

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,7 +3,7 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { getFirestore, Firestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 // Store the initialized services in a module-level variable to act as a singleton.
 let firebaseServices: { firebaseApp: FirebaseApp; auth: Auth; firestore: Firestore; } | null = null;
@@ -23,20 +23,26 @@ export function initializeFirebase(): { firebaseApp: FirebaseApp; auth: Auth; fi
 
   // On the client, we use the singleton pattern.
   if (!firebaseServices) {
-    if (!getApps().length) {
-      let firebaseApp;
-      try {
-        firebaseApp = initializeApp();
-      } catch (e) {
-         if (process.env.NODE_ENV === "production") {
-            console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+    const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+    const firestore = getFirestore(app);
+
+    try {
+        enableIndexedDbPersistence(firestore);
+    } catch (error: any) {
+        if (error.code == 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled in one tab at a time.
+            console.warn('Firestore persistence failed to enable. This is expected if you have multiple tabs open.');
+        } else if (error.code == 'unimplemented') {
+            // The current browser does not support all of the features required to enable persistence
+            console.warn('Firestore persistence is not supported in this browser.');
         }
-        firebaseApp = initializeApp(firebaseConfig);
-      }
-       firebaseServices = getSdks(firebaseApp);
-    } else {
-       firebaseServices = getSdks(getApp());
     }
+
+    firebaseServices = {
+        firebaseApp: app,
+        auth: getAuth(app),
+        firestore: firestore,
+    };
   }
 
   return firebaseServices;
